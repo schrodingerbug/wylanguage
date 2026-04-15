8e7b2d3626e6717fa9494e98e66bbecf1aa9c607
Schrodinger's Bug <kordonouris.nikos@gmail.com>
2026-04-15 18:30:59 +0300

    Made changes to Wy

--- script.py
+++ script.py
@@ -1,16 +1,40 @@
-var = input("Before printing,would you like to set a variable?If so,x=")
+var = None
+var_name = None
 
-output = input("Write your print code here:")
+def variable():
+  global var, var_name
+  var_name = input("Name your variable:")
+  var = input(f"{var_name}-)")
 
-if output != "log<'x'>":
- print(output[5:-2])
-elif output == "log<'x'>":
- print(var)
+def log():
+ output = input(">>")
+ if output != f"log<{var_name}>":
+  print(output[5:-2])
+ elif output == f"log<{var_name}>":
+  print(var)
 
-value = input("Check the value of x? x?=")
-if value == var:
-    print("True")
-elif value != var:
-    print("False")
+def boolean():
+ value = input(">>")
+ if value == f"{var_name}?={var}":
+     print("True")
+ elif value != f"{var_name}?={var}":
+     print("False")
 
+def maths():
+    global var,var_name
+    arithmetic = input(">>")
+    print(eval(arithmetic))
 
+while True:
+    command = input(">>")
+    if command == "variable":
+        variable()
+    elif command == "log":
+        log()
+    elif command == "boolean":
+        boolean()
+    elif command == "maths":
+        maths()
+    else:
+        print("Invalid input.")
+        break
